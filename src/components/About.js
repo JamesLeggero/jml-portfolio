9d71395c5b1ab0d66fbfe9c50afebf5a5e7d53aa
JamesLeggero <james.m.leggero@gmail.com>
2020-10-04 15:57:40 -0400

    added pic to ababout page

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,18 @@
 import React from 'react'
 
+import NewJML from './img/NewJML.png'
+
+const imgStyles = {
+    width: '400px',
+    border: '4px solid grey',
+    borderRadius: '100%'
+}
+
 export default function About() {
-    return <h1>ABOUT</h1>
+    return (
+        <>
+        <h1>ABOUT</h1>
+        <img src={NewJML} style={imgStyles} alt='JML' />
+        </>
+    ) 
 }
