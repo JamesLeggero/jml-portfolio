3540027afaac3aecbf87b9a6b4031163f7332382
JamesLeggero <james.m.leggero@gmail.com>
2020-10-14 11:06:16 -0400

    portfolio text mobile responsive, no hanging wrap words

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
-import Accordion from 'react-bootstrap/Accordion'
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
 
 export default function Projects() {
 
@@ -14,33 +11,38 @@ export default function Projects() {
     return (
         <>
         <h1 className='display-3'>pRojects</h1>
+        <p>written with html/css/javascript<br />
+        + listed additional technologies</p>
 
         <Container>
+        {
+        <>
+        <Container>
+            <h2 className='projects' id='https://jml-coffeetalk-client.herokuapp.com/' onClick={handleClick}>coffeetAlk</h2>
+            <h5>socket.io, webrtc, openweather,</h5><h5>textmagic, mern, bootstrap</h5>
+            <p>video chat in real time with<br/>
+                international coffee farmers</p>
+            </Container>
 
             <Container>
             <h2 className='projects' id='https://jamesleggero.github.io/pizzafreak/' onClick={handleClick}>piZZAfReAk</h2>
             <h5>jquery</h5>
-            <p>construct pizzas based on customers' favorite toppings</p>
-            </Container>
-
-            <Container>
-            <h2 className='projects' id='https://monkey-mania.herokuapp.com/' onClick={handleClick}>monkey mAniA</h2>
-            <h5>mern, bulma</h5>
-            <p>explore the world of banana-style modular synthesizers</p>
+            <p>construct pizzas based on<br />customers' favorite toppings</p>
             </Container>
 
             <Container>
             <h2 className='projects' id='https://harvard-art-museum.herokuapp.com/' onClick={handleClick}>HARVARd ARt MuseuM</h2>
-            <h5>harvard art museum api, mern, bootstrap</h5>
-            <p>curate a collection of your favorite art pieces</p>
+            <h5>harvard art museum api,</h5><h5>mern, bootstrap</h5>
+            <p>curate a collection of<br />your favorite art pieces</p>
             </Container>
 
             <Container>
-            <h2 className='projects' id='https://jml-coffeetalk-client.herokuapp.com/' onClick={handleClick}>coffeetAlk</h2>
-            <h5>socket.io, webrtc, openweather api, textmagic api, mern, bootstrap</h5>
-            <p>video chat in real time with international coffee farmers</p>
+            <h2 className='projects' id='https://monkey-mania.herokuapp.com/' onClick={handleClick}>Monkey MAniA</h2>
+            <h5>mern, bulma</h5>
+            <p>explore the world of<br />banana-style modular synthesizers</p>
             </Container>
-
+            </>
+}
 
             </Container>
         
