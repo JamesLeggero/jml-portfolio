de2ba42eeff91e4ed7bc766219d315b0f84cc673
JamesLeggero <james.m.leggero@gmail.com>
2020-10-16 10:06:31 -0400

    iframe version working

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,31 +1,40 @@
 import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
-import { Document, Page, pdfjs } from "react-pdf";
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import {Document, Page, pdfjs} from "react-pdf";
+import JML_Resume from './JML_Resume.pdf'
 
-const JML_Resume = require('./JML_Resume.pdf')
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 
 export default function Resume() {
 
-    const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
+  //   const [numPages, setNumPages] = useState(null);
+  // const [pageNumber, setPageNumber] = useState(1);
  
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+  // function onDocumentLoadSuccess({ numPages }) {
+  //   setNumPages(numPages);
+  // }
 
     return (
         <>
         <h1 className='display-3'>Resumé</h1>
-        <Container>
+
+        {/* {//works:}
+        <object data={JML_Resume} type="application/pdf" width="60%" height="1100px">
+          <p>Resume</p>
+          </object> */}
+
+          <iframe src={JML_Resume} title='JML_Resume' />
+       
+        {/* <Container>
             <ResponsiveEmbed>
-                <Document file='./JML_Resume.pdf'>
+                <Document file={JML_Resume}>
                     <Page pageNumber={1} />
                 </Document>
         </ResponsiveEmbed>
-        </Container>
+        </Container> */}
         </>
     )
 }
